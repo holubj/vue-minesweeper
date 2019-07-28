38508b7db9269d17a86222dd5c99d16dcb88516d
Jakub Holub <holubjakub@me.com>
2019-07-28 02:19:52 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,138 @@
+<template>
+  <div id="app">
+    <DifficultyForm
+      @handle-submitted="handleNew"
+      :defaultRows="rows"
+      :defaultCols="cols"
+      :defaultMines="mines"
+    />
+    <div id="info">
+      <Timer :active="isIngame" ref="timer" :key="'timer-' + timerKey" />
+      <div>
+        Mines left:
+        <strong>{{ minesLeft }}</strong>
+      </div>
+    </div>
+    <MineField
+      :rows="rows"
+      :cols="cols"
+      :mines="mines"
+      :key="'minefield-' + gameID"
+      @handle-win="handleWin"
+      @handle-lose="handleLose"
+      @handle-ingame="handleIngame"
+      @handle-flagged-mines="handleFlaggedMines"
+    />
+    <h2 v-if="result">{{result}}</h2>
+    <div id="protip">Protip: use space bar with mouse hover over cell for speedrunning.</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import MineField from './components/MineField.vue';
+import DifficultyForm from './components/DifficultyForm.vue';
+import Timer from './components/Timer.vue';
+
+enum State {
+  New,
+  Ingame,
+  Win,
+  Lose,
+}
+
+@Component({
+  components: {
+    DifficultyForm,
+    Timer,
+    MineField,
+  },
+})
+export default class App extends Vue {
+  public rows: number = 16;
+  public cols: number = 30;
+  public mines: number = 99;
+
+  public state: State = State.New;
+  public result: string = '';
+
+  public gameID: number = 1;
+  private flaggedMines: number = 0;
+
+  public handleNew(rows: number, cols: number, mines: number) {
+    if (rows < 3 || cols < 3 || mines < 1) {
+      return;
+    }
+
+    this.rows = rows;
+    this.cols = cols;
+    this.mines = mines;
+
+    this.state = State.New;
+    this.result = '';
+
+    this.flaggedMines = 0;
+    this.gameID += 1;
+  }
+
+  public handleIngame() {
+    this.state = State.Ingame;
+  }
+
+  public handleWin() {
+    this.state = State.Win;
+    this.result = 'Win!';
+    this.stopTimer();
+  }
+
+  public handleLose() {
+    this.state = State.Lose;
+    this.result = 'Game over!';
+    this.stopTimer();
+  }
+
+  public handleFlaggedMines(flaggedMines: number) {
+    this.flaggedMines = flaggedMines;
+  }
+
+  private stopTimer() {
+    // @ts-ignore
+    this.$refs.timer.stop();
+  }
+
+  get isIngame() {
+    return this.state === State.Ingame;
+  }
+
+  get isLoss() {
+    return this.state === State.Lose;
+  }
+
+  get timerKey() {
+    return this.state !== State.New;
+  }
+
+  get minesLeft() {
+    return this.mines - this.flaggedMines;
+  }
+}
+</script>
+
+<style lang="less">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 60px;
+}
+
+#info {
+  margin-bottom: 15px;
+}
+
+#protip {
+  margin-top: 15px;
+  color: #666;
+}
+</style>
---
+++ src/components/DifficultyForm.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="difficulty-form">
+    <input type="text" v-model.number="rows" />
+    x
+    <input type="text" v-model.number="cols" />
+    <br />
+    <label for>#mines:</label>
+    <input type="text" v-model.number="mines" />
+    <br />
+    <button @click="handleSubmit">New Game</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class DifficultyForm extends Vue {
+  @Prop() public defaultRows!: number;
+  @Prop() public defaultCols!: number;
+  @Prop() public defaultMines!: number;
+
+  public rows: number = this.defaultRows;
+  public cols: number = this.defaultCols;
+  public mines: number = this.defaultMines;
+
+  public handleSubmit(e: any) {
+    e.target.blur();
+    this.$emit('handle-submitted', this.rows, this.cols, this.mines);
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+#difficulty-form {
+  margin-bottom: 15px;
+}
+
+input[type='text'] {
+  width: 50px;
+  text-align: center;
+}
+
+label {
+  margin-right: 5px;
+  font-size: 12px;
+}
+</style>
